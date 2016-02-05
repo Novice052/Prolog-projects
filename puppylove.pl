0c6b0c8a3449f4eb912444eccc59d251b9dd8d00
John <jabbott1@asu.edu>
2016-02-05 16:29:03 -0700

    updadte

--- puppylove.pl
+++ puppylove.pl
@@ -1,11 +1,8 @@
-right(X,Y,L):- 	append(_,[X,Y|_],L).
-next(X,Y,L):- 	right(X,Y,L) ; right(Y,X,L).
-
 start(Sol):- length(Sol,4),
 	member([lauren,trouper,_],Sol),				%1
 	member([nicky,_,terrier],Sol),				%2
 	member([_,frisky,poodle],Sol),				%3
-	next([terry,_,_],[_,_,collie],Sol),			%4a
-	next([terry,_,_],[_,_,retriever],Sol),			%4b
-	next([_,happy,_],[robin,_,_],Sol),			%5
-	next([_,wiggles,_],[_,_,collie],Sol).			%6
+	\+ member([terry,_,collie],Sol),				%4a
+	\+ member([terry,_,retriever],Sol),			%4b
+	\+ member([robin,happy,_],Sol),				%5
+	\+ member([_,wiggles,collie],Sol).			%6
